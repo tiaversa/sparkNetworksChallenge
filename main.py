6ade583bfd1b3611256fab119a5832c5b0124332
tiaversa <75630776+tiaversa@users.noreply.github.com>
2021-12-07 14:47:20 -0700

    tests with readme file

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from etl import *
 
+# Extraction by requests
 jsonRespUser = make_request('https://619ca0ea68ebaa001753c9b0.mockapi.io/evaluation/dataengineer/jr/v1/users')
 jsonRespMessages = make_request('https://619ca0ea68ebaa001753c9b0.mockapi.io/evaluation/dataengineer/jr/v1/messages')
 
-
+# Transformation of data
 df_users = creating_user_df(jsonRespUser)
-print(df_users)
-
 df_subscriptions = creating_subscriptions_df(jsonRespUser)
-print(df_subscriptions)
-
 df_messages = creating_messages_df(jsonRespMessages)
-print(df_messages)
 
+
+# Load of the data into the dataBase
 engine = engine_connect_db()
-
 load_user_df(engine, df_users)
-
 load_subscriptions_df(engine, df_subscriptions)
-
 load_messages_df(engine, df_messages)
