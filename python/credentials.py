7ef8cf5a2db2d9116e550ef6bd32fa86ed1223d4
tiaversa <75630776+tiaversa@users.noreply.github.com>
2021-12-10 06:18:01 -0700

    A working Docker!!!!!
    This is the adding for a working docker for the project.

--- python/credentials.py
+++ python/credentials.py
@@ -1,16 +1,7 @@
-# MySQL (source db)
-#mysql_db_config = {
-#    'host': 'mysql_server',
-#    'user': 'ti',
-#    'password': 'secret',
-#    'port': 3308,
-#    'database' : 'spark_networks_test'
-#}
-
 mysql_db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '*Saltlake5',
+    'host': 'mysql_db',
+    'user': 'ti',
+    'password': 'secret',
     'port': 3306,
     'database' : 'spark_networks_test'
 }
